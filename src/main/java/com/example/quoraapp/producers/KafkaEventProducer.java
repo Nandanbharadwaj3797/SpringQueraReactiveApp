e755c24fa2dda0e99bb673a7d993b3cb83191c76
nandan kumar <nandanbharadwaj4@gmail.com>
2025-09-10 17:14:22 +0530

    Added Kafka consumer But Each Refresh viewCount Increase

--- src/main/java/com/example/quoraapp/producers/KafkaEventProducer.java
+++ src/main/java/com/example/quoraapp/producers/KafkaEventProducer.java
@@ -1,10 +1,12 @@
 package com.example.quoraapp.producers;
 
-import com.example.quoraapp.config.kafkaConfig;
-import com.example.quoraapp.events.ViewCountEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import com.example.quoraapp.config.KafkaConfig;
+import com.example.quoraapp.events.ViewCountEvent;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -13,12 +15,11 @@ public class KafkaEventProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void publishViewCountEvent(ViewCountEvent viewCountEvent) {
-        kafkaTemplate.send(kafkaConfig.Topic_Name, viewCountEvent.getTargetId(), viewCountEvent)
-                .whenComplete((result,err) -> {
-                    if (err != null) {
-                        System.out.println("Error publishing view count event"+err.getMessage());
+        kafkaTemplate.send(KafkaConfig.TOPIC_NAME, viewCountEvent.getTargetId(), viewCountEvent)
+                .whenComplete((result, err) -> {
+                    if(err != null) {
+                        System.out.println("Error publishing view count event: " + err.getMessage());
                     }
                 });
     }
-
 }
